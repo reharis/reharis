2a5bb97aca042cc63ee04c7e904687b888c3cf9f
Muhammad Haris <mhkcodes@gmail.com>
2024-08-06 18:49:50 +0500

    🚀

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-
-export default defineConfig({
-  site: "https://haris2k.com",
-  integrations: [mdx(), sitemap(), tailwind()],
-});
